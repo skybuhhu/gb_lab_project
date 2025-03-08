21446d67bb8e2646c40c2753588b6220dabe8a3b
pavel nebolsin <pgnebolsin@gmail.com>
2025-03-08 13:51:03 +0300

    five

--- less3/keyboards/prof_button.py
+++ less3/keyboards/prof_button.py
@@ -9,16 +9,17 @@ from aiogram import types
 
 def make_row_keyboard(items: list[str]) -> types.ReplyKeyboardMarkup:
     """
-    Создает клавиатуру с кнопками, включая кнопку "Отмена".
+    Создает клавиатуру с кнопками, группируя их по 3 в строку.
+    Кнопка "Отмена" всегда на отдельной строке.
     """
     # Создаем копию списка, чтобы не изменять исходный
     buttons = [types.KeyboardButton(text=item) for item in items]
 
-    # Добавляем кнопку "Отмена" в конец
-    buttons.append(types.KeyboardButton(text="Отмена"))
+    # Группируем кнопки по 3 в строку
+    keyboard = [buttons[i:i + 3] for i in range(0, len(buttons), 3)]
 
-    # Группируем кнопки в строки (по одной кнопке в строке)
-    keyboard = [[button] for button in buttons]
+    # Добавляем кнопку "Отмена" на отдельной строке
+    keyboard.append([types.KeyboardButton(text="Отмена")])
 
     # Возвращаем клавиатуру
     return types.ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
